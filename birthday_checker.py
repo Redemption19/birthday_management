739815338752fc52d8b98350a8e5b2d28909a56d
Bismark Abban <abbanb2@gmail.com>
2025-05-13 15:46:11 +0000

    Refactor birthday reminder function to use check_and_send_birthday_reminders for improved functionality

--- birthday_checker.py
+++ birthday_checker.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.email_service import check_upcoming_birthdays
+from utils.email_service import check_and_send_birthday_reminders
 from utils.database import init_connection
 
 def main():
@@ -7,7 +7,7 @@ def main():
     init_connection()
     
     # Check and send birthday reminders
-    check_upcoming_birthdays()
+    check_and_send_birthday_reminders()
 
 if __name__ == "__main__":
-    main() 
+    main()
